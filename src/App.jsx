346fb0467fb5b2323d310b90e432842cf898c24c
Shubham <shubh786hacker@gmail.com>
2025-10-27 17:09:01 +0530

    Added speedinsights of vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./Components/navbar";
 import Sidebar from "./Components/sidebar";
 import Dashboard from "./Components/Dashboard";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <>
       <Navbar />
       <div className="main flex"><Sidebar/><Dashboard/></div>
+      <SpeedInsights />
     </>
   );
 }
